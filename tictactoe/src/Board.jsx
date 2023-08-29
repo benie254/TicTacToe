3ebba7337dbc1507dff3038117145546baddeb2c
benie254 <davinci.monalissa@gmail.com>
2023-08-29 11:26:34 +0300

    declare a new state variable

--- tictactoe/src/Board.jsx
+++ tictactoe/src/Board.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
 function Square() {
-    const [value, setValue] = useState(null);
+    // declare a state variable 'squares' 
+    // that defaults to an array of 9 nulls 
+    // corresponding to the existing 9 squares
+    const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick() {
         setValue('X');
